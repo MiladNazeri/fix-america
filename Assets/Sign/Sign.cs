415c5490cc5685491a6b5a40ad5da3f963bc7160
cameron <cameron.loui@gmail.com>
2020-02-01 15:49:16 -0800

    add sign, text mesh pro

---
+++ Assets/Sign/Sign.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject text; 
+    public GameObject paper;
+
+    public enum Size 
+    {
+        Small, Medium, Large
+    }
+
+    public List<Vector3> textSizes;
+    public List<Vector3> paperSizes;
+
+    public void SetSize () 
+    {
+
+    }
+    
+    public void SetText()
+    {
+
+    }
+}
