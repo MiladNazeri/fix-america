5064af04f636bcc765206e36888fcdd4aa811fed
Ari Romano McBride <git@somian.net>
2020-02-01 13:45:47 -0800

    basic protester spawning

---
+++ Assets/Protesters/ProtesterSpawn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtesterSpawn : MonoBehaviour
+{
+	 void OnDrawGizmos()
+    {
+
+        Gizmos.DrawIcon(transform.position, "SpawnPoint.png", false);
+    }
+}
