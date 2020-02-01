b5a56ae9be49075b71cea682a2e7076ce25e02e3
tdubourg <notfound@undefined.com>
2020-02-01 15:13:48 -0800

    work in progress on TV within backend test scene

---
+++ Assets/Scripts/TVController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVController : MonoBehaviour
+{
+    public static TVController Instance { get; private set; }
+
+    private void Awake() {
+        if (null == Instance) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            // Another one already exists, suicide ourselves, there can only be one
+            Destroy(gameObject);
+        }
+    }
+
+    public void DisplayText(string text)
+    {
+    	
+    }
+}
