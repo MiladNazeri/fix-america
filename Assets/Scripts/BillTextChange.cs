118f46095fdc5156b932f97a9591c206aa572279
cameron <cameron.loui@gmail.com>
2020-02-01 17:12:34 -0800

    add text to bills

---
+++ Assets/Scripts/BillTextChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BillTextChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro text;
+
+    public void SetText(string t)
+    {
+        text.SetText(t);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
