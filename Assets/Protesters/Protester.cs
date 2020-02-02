441ff3bad2cf4406e4453c8e23bc8ca66ed4bf76
Ari Romano McBride <git@somian.net>
2020-02-01 19:05:23 -0800

    protesters can now stop moving

--- Assets/Protesters/Protester.cs
+++ Assets/Protesters/Protester.cs
@@ -9,6 +9,12 @@ public class Protester : MonoBehaviour
 	//state stuff
 
 	public bool despawning;
+	public bool arrived;
+
+	public Vector3 lastPosition;
+	float lastCheckTime;
+	public float positionCheckInterval = 3.0f;
+	public float minWalkCheckDistance = 10.0f;
 
 	NavMeshAgent agent;
 	void Awake () {
@@ -18,7 +24,14 @@ public class Protester : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		if (Time.time - positionCheckInterval > lastCheckTime){
+			lastCheckTime = Time.time;
+			if(Vector3.Distance(transform.position, lastPosition) < minWalkCheckDistance){
+				arrived = true;
+				agent.isStopped = true;
+			}
+			lastPosition = transform.position;
+		}
     }
 
 	public void SetDestination(Vector3 destination){
